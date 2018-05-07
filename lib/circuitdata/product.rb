12c87e8c02b945df7a13f5184780f0cd2b369b54
Max Brosnahan <maximilianbrosnahan@gmail.com>
2018-05-07 17:08:14 +0200

    Handle integer path components
    So that arrays can be indexed as well.

--- lib/circuitdata/product.rb
+++ lib/circuitdata/product.rb
@@ -57,7 +57,7 @@ module Circuitdata
 
     def question_answer(path)
       return nil if path.empty?
-      path = path.map(&:to_sym)
+      path = path.map { |p| p.is_a?(String) ? p.to_sym : p }
       value = product_data.dig(*path)
       value
     end
