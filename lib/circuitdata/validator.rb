30ca8943b8115236708c8c23c9525c7c872b00cb
Max Brosnahan <maximilianbrosnahan@gmail.com>
2018-05-07 17:35:18 +0200

    Extract JSON error parsing into a separate class
    Why: So that it is easier to extend and add tests for.

--- lib/circuitdata/validator.rb
+++ lib/circuitdata/validator.rb
@@ -1,3 +1,5 @@
+require_relative './validator/json_schema_error_parser'
+
 module Circuitdata
   class Validator
     attr_reader :errors
@@ -24,35 +26,8 @@ module Circuitdata
 
     def errors=(schema_errors)
       @errors = schema_errors.map do |error|
-        additional_data = extract_data(error[:message], error[:failed_attribute])
-        path = error[:fragment].gsub('#', '')
-        {
-          source_path: path,
-          field: path.split('/').last,
-        }.merge(additional_data)
+        JsonSchemaErrorParser.translate(error)
       end
-    end
-
-    def extract_data(message, failed_attribute)
-      case failed_attribute
-      when "Required"
-        field = message.match(/of '(.*)'/)[1]
-        if !field
-          fail "Unable to extract field from #{message.inspect}"
-        end
-        return { field: field, problem: 'required_property_missing' }
-      when "TypeV4"
-        if message.include?("did not match the following type")
-          matches = message.match(/of type (\S*) did not match the following type: (\S*)/)
-          actual, expected = matches[1..2]
-          return { actual: actual, expected: expected, problem: 'type_mismatch' }
-        end
-      when 'AdditionalProperties'
-        matches = message.match(/contains additional properties (\[.*\]) outside/)
-        additional_properties = JSON.parse(matches[1])
-        return { additional_properties: additional_properties, problem: 'additional_properties' }
-      end
-      fail "Unhandled error message: #{message.inspect}"
     end
   end
 end
---
+++ lib/circuitdata/validator/json_schema_error_parser.rb
@@ -0,0 +1,39 @@
+module Circuitdata
+  class Validator
+    class JsonSchemaErrorParser
+      class<<self
+        def translate(error)
+          additional_data = extract_data(error[:message], error[:failed_attribute])
+          fail "Unhandled error: #{error.inspect}" if additional_data.nil?
+
+          path = error[:fragment].gsub('#', '')
+          {
+            source_path: path,
+            field: path.split('/').last,
+          }.merge(additional_data)
+        end
+
+        def extract_data(message, failed_attribute)
+          case failed_attribute
+          when "Required"
+            field = message.match(/of '(.*)'/)[1]
+            if !field
+              fail "Unable to extract field from #{message.inspect}"
+            end
+            return { field: field, problem: 'required_property_missing' }
+          when "TypeV4"
+            if message.include?("did not match the following type")
+              matches = message.match(/of type (\S*) did not match the following type: (\S*)/)
+              actual, expected = matches[1..2]
+              return { actual: actual, expected: expected, problem: 'type_mismatch' }
+            end
+          when 'AdditionalProperties'
+            matches = message.match(/contains additional properties (\[.*\]) outside/)
+            additional_properties = JSON.parse(matches[1])
+            return { additional_properties: additional_properties, problem: 'additional_properties' }
+          end
+        end
+      end
+    end
+  end
+end
