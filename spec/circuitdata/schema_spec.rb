779c51831a83d2b13c8f8bffe810225921d868b1
Max Brosnahan <maximilianbrosnahan@gmail.com>
2018-08-09 13:21:18 +0200

    Move spec related tests to schema

--- spec/circuitdata/schema_spec.rb
+++ spec/circuitdata/schema_spec.rb
@@ -95,5 +95,71 @@ RSpec.describe Circuitdata::Schema do
       cached = subject.profile_questions
       expect(cached).to eql(uncached)
     end
+
+    context "question matches expected structure" do
+      let(:expected_structure) {
+        {
+          :id => "sections",
+          :name => "Sections",
+          :array? => false,
+          :questions => [
+            {
+              :id => "sections/count",
+              :code => "count",
+              :name => "Count",
+              :description => "",
+              :defaults => {
+                :schema => {
+                  :type => "integer",
+                },
+                :path => "/open_trade_transfer_package/profiles/defaults/circuitdata/sections/count",
+              },
+              :uom => nil,
+              :required => {
+                :schema => {:type => "integer"},
+                :path => "/open_trade_transfer_package/profiles/required/circuitdata/sections/count",
+              },
+              :forbidden => {
+                :schema => {:type => "integer"},
+                :path => "/open_trade_transfer_package/profiles/forbidden/circuitdata/sections/count",
+              },
+            },
+            {
+              :id => "sections/mm2",
+              :code => "mm2",
+              :name => "Mm2",
+              :description => "",
+              :defaults => {
+                :schema => {:type => "number"},
+                :path => "/open_trade_transfer_package/profiles/defaults/circuitdata/sections/mm2",
+              },
+              :uom => nil,
+              :required => {
+                :schema => {:type => "number"},
+                :path => "/open_trade_transfer_package/profiles/required/circuitdata/sections/mm2",
+              },
+              :forbidden => {
+                :schema => {:type => "number"},
+                :path => "/open_trade_transfer_package/profiles/forbidden/circuitdata/sections/mm2",
+              },
+            },
+          ],
+        }
+      }
+
+      it "returns the correct structure" do
+        result = subject.profile_questions.first
+        expect(result.except(:questions)).to eql(expected_structure.except(:questions))
+        e_qs = expected_structure.fetch(:questions)
+        r_qs = result.fetch(:questions)
+        expect(r_qs.first).to eql(e_qs.first)
+        expect(r_qs.second).to eql(e_qs.second)
+      end
+
+      it "does not have any nested objects" do
+        result = JSON.generate(subject.profile_questions)
+        expect(result).not_to include('"type":"object"')
+      end
+    end
   end
 end
