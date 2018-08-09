a61ff941d9f17b82070495c4a150cbd3047efff8
Max Brosnahan <maximilianbrosnahan@gmail.com>
2018-08-09 13:31:42 +0200

    Add empty profile initialization

--- lib/circuitdata/profile.rb
+++ lib/circuitdata/profile.rb
@@ -1,7 +1,31 @@
 module Circuitdata
   class Profile
+    BASIC_PROFILE_STRUCTURE = {
+      open_trade_transfer_package: {
+        version: SCHEMA_VERSION,
+        profiles: {
+          default: {circuitdata: {version: SCHEMA_VERSION}},
+          restricted: {circuitdata: {version: SCHEMA_VERSION}},
+          enforced: {circuitdata: {version: SCHEMA_VERSION}},
+        },
+      },
+    }
     def self.questions
       Schema.profile_questions
+    end
+
+    def initialize(data:)
+      @data = data
+    end
+
+    def data
+      @data ||= setup_basic_data
+    end
+
+    private
+
+    def setup_basic_data
+      BASIC_PROFILE_STRUCTURE.deep_dup
     end
   end
 end
--- spec/circuitdata/profile_spec.rb
+++ spec/circuitdata/profile_spec.rb
@@ -1,5 +1,17 @@
 require "spec_helper"
 
 RSpec.describe Circuitdata::Profile do
-  subject { described_class }
+  subject { described_class.new(data: data) }
+  let(:data) { nil }
+
+  describe "profile initialization" do
+    let(:expected_result) { json_fixture(:empty_profile) }
+
+    it "creates a valid empty profile" do
+      expect(subject.data).to eql(expected_result)
+
+      validator = Circuitdata::Validator.new(subject.data)
+      expect(validator).to be_valid
+    end
+  end
 end
