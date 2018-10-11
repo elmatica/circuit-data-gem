e60d131943e01857593c14d3754064781e7b32c3
Max Brosnahan <maximilianbrosnahan@gmail.com>
2018-10-11 10:00:18 +0200

    Add back error spec

---
+++ spec/circuitdata/json_validator/json_schema_error_parser_spec.rb
@@ -0,0 +1,43 @@
+require "spec_helper"
+
+RSpec.describe Circuitdata::JsonValidator::JsonSchemaErrorParser do
+  it 'handles required properties' do
+    errors = generate_errors_and_translate(
+      {},
+      {type: "object", required: ["test"], properties: {test: {type: "string"}}}
+    )
+    expect(errors).to eql([{:source_path => "/", :field => "test", :problem => "required_property_missing"}])
+  end
+
+  it 'handles type mismatches' do
+    errors = generate_errors_and_translate(
+      {test: 1},
+      {type: "object", properties: {test: {type: "string"}}}
+    )
+    expect(errors).to eql([{:source_path => "/test", :field => "test", :actual => "integer", :expected => "string", :problem => "type_mismatch"}])
+  end
+
+  it 'handles additional properties' do
+    errors = generate_errors_and_translate(
+      {test: 1},
+      {type: "object", additionalProperties: false, properties: {}}
+    )
+    expect(errors).to eql([{:source_path => "/", :field => nil, :additional_properties => ["test"], :problem => "additional_properties"}])
+  end
+
+  it 'handles enums' do
+    errors = generate_errors_and_translate(
+      {test: "coffee"},
+      {type: "object", properties: {test: {type: "string", enum: ["tea"]}}}
+    )
+    expect(errors).to eql([{:source_path => "/test", :field => "test", :problem => "not_in_enum"}])
+  end
+
+  def generate_errors_and_translate(data, schema)
+    errors = JSON::Validator.fully_validate(
+      schema, data, errors_as_objects: true,
+    )
+    fail "no errors generated" if errors.count == 0
+    errors.map { |e| described_class.translate(e) }
+  end
+end
